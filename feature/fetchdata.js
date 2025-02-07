44e5433de9702b278afb10eac478512d6348b815
Younes <younesaly207@gmail.com>
2025-02-07 20:26:43 +0200

    Initial Commit

---
+++ feature/fetchdata.js
@@ -0,0 +1,8 @@
+fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=2025-07-02&end_date=2025-07-09&api_key=PbcCHNTY5pNdiASzfupkHuPSiEJEJ9Q2OKzMEc7M`)
+.then(data => data.json())
+.then(data =>{
+    console.log(data);
+    return data;
+})
+.catch(error => console.log(error));
+
